f163671b83abc4e6839b20e93da30b204f68614a
Baldwin <Baldwincepeda@gmail.com>
2023-02-17 13:21:15 -0500

    wallet connect modal

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import WebModal from 'web3modal';
+import { ethers } from "ethers";
+import { CoinbaseWalletSDK } from "@coinbase/wallet-sdk";
+
+const providerOptions = {
+
+  coinbasewallet: {
+    //@Baldwin add coibase wallet 
+    package: CoinbaseWalletSDK,
+    Options: {
+      appName: "LendVest Credit Score",
+      infuraId: { 3: "" }
+    }
+
+
+  }
+
+
+}
 
 function App() {
+
+  async function connectWallet() {
+
+    try {
+      let web3Modal = new WebModal({
+        cacheProvider: false,
+        providerOptions,
+      });
+      //connecting with the wallet 
+      const web3ModalInstance = await web3Modal.connect();
+      //allows us to interact with the wallet 
+      const web3ModalProvider = new ethers.providers.Web3Provider(web3ModalInstance);
+      //debug
+      console.log(web3ModalProvider);
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>Web3Modal Connection</h1>
+        <button onClick={connectWallet}>Connect Wallet</button>
       </header>
     </div>
   );
---
+++ src/components/tokenComp/index.js
@@ -0,0 +1,5 @@
+
+
+function TokenComp(params) {
+
+}
